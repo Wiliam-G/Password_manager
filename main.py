f643df9966f155104c8706009beebfdf2b346129
Wiliam-G <wiliam.gomes99@gmail.com>
2024-11-10 20:22:52 -0300

    Add functionality to button Add, also adds a check for empty website and password

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    website = website_entry.get()
+    email = email_user_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please fill all the fields.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered \n Email: {email}\n"
+                                                      f"Password: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -19,12 +38,14 @@ website_label.grid(row=1, column=0)
 
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="w",padx=5, pady=5)
+website_entry.focus()
 
 email_username_label = Label(text="Email/Username")
 email_username_label.grid(row=2, column=0)
 
 email_user_entry = Entry(width=35)
 email_user_entry.grid(row=2, column=1, columnspan=2, sticky="w", padx=5, pady=5)
+email_user_entry.insert(0, "wiliam.gomes99@gmail.com")
 
 password_label = Label(text="Password")
 password_label.grid(row=3, column=0)
@@ -35,7 +56,7 @@ password_entry.grid(row=3, column=1, sticky="w", padx=5, pady=5)
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=1, sticky="E", pady=5 )
 
-add_button = Button(text="Add", width=30)
+add_button = Button(text="Add", width=30, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="w", pady=5)
 
 
